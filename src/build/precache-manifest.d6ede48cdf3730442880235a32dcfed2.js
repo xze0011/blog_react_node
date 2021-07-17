d88fbe4d0d26b43665f9074c9f42c69304450916
1023002480 <xzen0011@studnet.monash.edu>
2021-07-17 20:58:28 +1000

    blog finished

---
+++ src/build/precache-manifest.d6ede48cdf3730442880235a32dcfed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d1e10226d42e3fcc67e",
+    "url": "/static/css/main.cb561371.chunk.css"
+  },
+  {
+    "revision": "0d1e10226d42e3fcc67e",
+    "url": "/static/js/main.7ad3ff50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9bba16425ed883b1b008",
+    "url": "/static/js/2.2f36f74c.chunk.js"
+  },
+  {
+    "revision": "7885c80df23a0840774bb4b62c3f9976",
+    "url": "/index.html"
+  }
+];
